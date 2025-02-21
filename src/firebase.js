eb06e01d9934e16801ebfe51ece962d644684a45
mannianurag <mannianurag@gmail.com>
2025-02-21 16:55:11 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyA1DW4HNBsjZoRRy2tm5Fd3gq2IFisAu7I",
   authDomain: "reversewordle-c90bb.firebaseapp.com",
@@ -8,7 +9,7 @@ const firebaseConfig = {
   storageBucket: "reversewordle-c90bb.firebasestorage.app",
   messagingSenderId: "912945406275",
   appId: "1:912945406275:web:ea7b37e9e36b1b88e74ff6",
-  measurementId: "G-HL7MVET68G",
+  measurementId: "G-HL7MVET68G"
 };
 
 const app = initializeApp(firebaseConfig);
