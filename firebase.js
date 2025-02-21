d083df0cf0f48c70a3eae98416b9d5829d5afa54
mannianurag <mannianurag@gmail.com>
2025-02-21 16:26:29 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA1DW4HNBsjZoRRy2tm5Fd3gq2IFisAu7I",
+  authDomain: "reversewordle-c90bb.firebaseapp.com",
+  projectId: "reversewordle-c90bb",
+  storageBucket: "reversewordle-c90bb.firebasestorage.app",
+  messagingSenderId: "912945406275",
+  appId: "1:912945406275:web:ea7b37e9e36b1b88e74ff6",
+  measurementId: "G-HL7MVET68G",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
